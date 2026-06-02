4a87e01634b82413f46a4481116be0b536e62127
Nano <jonathan.wenan@gmail.com>
2026-06-02 15:50:45 +0800

    Update SAC_app.py
    Added comments & print statements (for progress check)

--- v4e/SAC_app.py
+++ v4e/SAC_app.py
@@ -30,6 +30,7 @@ def set_seed(seed=42):
         torch.cuda.manual_seed_all(seed) # 若有多張 GPU
     
     # 針對卷積運算的決定性設定 (注意：這可能會稍微降低運算效能)
+    # force gpu to be deterministic (may slow down training)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     
@@ -45,6 +46,7 @@ def preprocess(state_img, dashboard_vec, device):
     
     # 建立一個與 dashboard_vec 同形狀的 scale 向量
     # 速度上限 100, 輪速上限 230
+    # normalize to [0,1] for each element
     scales = torch.FloatTensor([100.0, 230.0, 230.0, 230.0, 230.0, 1.0, 1.0, 1.0]).to(device)
     
     # 轉換為 Tensor 並直接除以各自的上限
@@ -128,122 +130,137 @@ def evaluate_agent(agent, seed, num_trials):
     rewards = np.array(rewards)
     return rewards.mean(), rewards.max(), rewards.min()
 
-env = gym.make("CarRacing-v3", render_mode="rgb_array", domain_randomize=False, continuous=True, max_episode_steps=5000, lap_complete_percent=0.99)
-env.action_space.seed(SEED)
-env.observation_space.seed(SEED)
-env.reset(seed=SEED)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if __name__ == "__main__":
+    env = gym.make("CarRacing-v3", render_mode="rgb_array", domain_randomize=False, continuous=True, max_episode_steps=5000, lap_complete_percent=0.99)
+    env.action_space.seed(SEED)
+    env.observation_space.seed(SEED)
+    env.reset(seed=SEED)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-max_buffer_size = 500000
-min_buffer_size = 10000
-num_total_steps = 1000000
-num_episode_step = 5000
-batch_size = 128
+    max_buffer_size = 500000
+    min_buffer_size = 10000
+    num_total_steps = 1000000
+    num_episode_step = 5000
+    batch_size = 128
 
-buffer = PrioritizedReplayBuffer(max_buffer_size)
-agent = SACAgent(device)
+    buffer = PrioritizedReplayBuffer(max_buffer_size)
+    agent = SACAgent(device)
 
-checkpoint_path = "sac_car_checkpoint.pth"
-buffer_path = f"sac_car_buffer.pkl"
-total_steps = 0
-now_score = 0
-best_score = float('-inf')
-mean_rewards = []
-max_rewards = []
-min_rewards = []
+    checkpoint_path = "sac_car_checkpoint.pth"
+    buffer_path = f"sac_car_buffer.pkl"
+    total_steps = 0
+    now_score = 0
+    best_score = float('-inf')
+    mean_rewards = []
+    max_rewards = []
+    min_rewards = []
 
-if (os.path.exists(checkpoint_path)):
-    load_buffer(buffer, buffer_path)
-    total_steps, now_score, best_score = agent.load_checkpoint(checkpoint_path)
-    total_steps += 1
-
-for episode in range(total_steps):
-    state, info = env.reset()
-
-while (True):
-    state, info = env.reset()
-    for t in range(50):
-        env.step(np.array([0, 0, 0]))
-    state = state[2:82]
-    dashboard = np.zeros(8)
-    episode_reward = 0
-    stay_neg = 0
-    
-    for t in range(num_episode_step):
-        # 選擇動作
-        img_t, dash_t = preprocess(state, dashboard, device)
-        with torch.no_grad():
-            action, _ = agent.actor.sample(img_t, dash_t)
-        action = action.cpu().numpy()[0]
-
-        # 執行動作
-        next_state, reward, done, truncated, info = env.step(action)
-        next_state = next_state[2:82]
-        
-        # 更新物理量 (Dashboard)
-        car = env.unwrapped.car
-        next_dashboard = np.concat((np.array([np.linalg.norm(car.hull.linearVelocity)] + [w.omega for w in car.wheels]), action))
-        
-        # 判斷卡死
-        if (reward<0):
-          stay_neg += 1
-        else:
-          stay_neg = 0
-        if (stay_neg>500):
-          done = True
-        
-        # 判斷飛出去
-        on_grass = True
-        for wheel in env.unwrapped.car.wheels:
-            if len(wheel.tiles) > 0:
-                on_grass = False
-                break
-        if on_grass:
-          done = True
-        
-        # 存入 Buffer
-        buffer.push(state, dashboard, action, reward, next_state, next_dashboard, done)
-        
-        state, dashboard = next_state, next_dashboard
-        episode_reward += reward
-    
+    if (os.path.exists(checkpoint_path)):
+        load_buffer(buffer, buffer_path)
+        total_steps, now_score, best_score = agent.load_checkpoint(checkpoint_path)
         total_steps += 1
-        
-        # 訓練模型
-        if len(buffer) >= min_buffer_size:
-        
-            if (total_steps%1000 == 0):
-                print(f"total_steps: {total_steps}")
-    
-                now_mean, now_max, now_min = evaluate_agent(agent, SEED+100, 5)
-                best_score = max(best_score, now_mean)
-    
-                mean_rewards.append(now_mean)
-                max_rewards.append(now_max)
-                min_rewards.append(now_min)
-    
-                df = pd.DataFrame({
-                    'mean': mean_rewards,
-                    'max': max_rewards,
-                    'min': min_rewards
-                })
-    
-                df.to_csv('training_cruve.csv', index=False, encoding='utf-8-sig')
-    
-                agent.save_checkpoint(checkpoint_path, total_steps, now_mean, best_score)
-                save_buffer(buffer, buffer_path)
-                
-                if (now_mean==best_score):
-                    os.system(f"cp {checkpoint_path} sac_car_best_model.pth")
 
-            agent.update_parameters(buffer, batch_size)
+        print(f"Resuming from checkpoint...\n \
+              step = {total_steps}\n\
+              last_score = {now_score:.1f}\n\
+              best_score = {best_score:.1f}\n\
+              buffer_size = {len(buffer)}\n")
+    else:
+        print("No saved checkpoint found, start from beginning...")
+
+    for episode in range(total_steps):
+        state, info = env.reset()
+
+
+    while (True):
+        state, info = env.reset()
+        for t in range(50):
+            env.step(np.array([0, 0, 0]))
+        state = state[2:82]
+        dashboard = np.zeros(8)
+        episode_reward = 0
+        stay_neg = 0
+    
+        for t in range(num_episode_step):
+            # 選擇動作
+            img_t, dash_t = preprocess(state, dashboard, device)
+            with torch.no_grad():
+                action, _ = agent.actor.sample(img_t, dash_t)
+            action = action.cpu().numpy()[0]
+
+            # 執行動作
+            next_state, reward, done, truncated, info = env.step(action)
+            next_state = next_state[2:82]
+            
+            # 更新物理量 (Dashboard)
+            car = env.unwrapped.car
+            next_dashboard = np.concatenate((np.array([np.linalg.norm(car.hull.linearVelocity)] + [w.omega for w in car.wheels]), action))
+            
+            # 判斷卡死
+            if (reward<0):
+                stay_neg += 1
+            else:
+                stay_neg = 0
+
+            if (stay_neg>500):
+                print(f"[Step {t}] Early stop: negative steps")
+                done = True
+            
+            # 判斷飛出去
+            on_grass = True
+            for wheel in env.unwrapped.car.wheels:
+                if len(wheel.tiles) > 0:
+                    on_grass = False
+                    break
+            if on_grass:
+                print(f"[Step {t}] Early stop: car on grass")
+                done = True
+            
+            # 存入 Buffer
+            buffer.push(state, dashboard, action, reward, next_state, next_dashboard, done)
+            
+            state, dashboard = next_state, next_dashboard
+            episode_reward += reward
+        
+            total_steps += 1
+            
+            # 訓練模型
+            if len(buffer) >= min_buffer_size:
+            
+                if (total_steps%1000 == 0):
+                    now_mean, now_max, now_min = evaluate_agent(agent, SEED+100, 5)
+                    best_score = max(best_score, now_mean)
+
+                    print(f"Total steps: {total_steps}, now_mean={now_mean:.1f}  max={now_max:.1f}  min={now_min:.1f}  best={best_score:.1f}")
+
+                    mean_rewards.append(now_mean)
+                    max_rewards.append(now_max)
+                    min_rewards.append(now_min)
+        
+                    df = pd.DataFrame({
+                        'mean': mean_rewards,
+                        'max': max_rewards,
+                        'min': min_rewards
+                    })
+        
+                    df.to_csv('training_curve.csv', index=False, encoding='utf-8-sig')
+        
+                    agent.save_checkpoint(checkpoint_path, total_steps, now_mean, best_score)
+                    save_buffer(buffer, buffer_path)
+                    
+                    if (now_mean==best_score):
+                        print(f"New best model saved")
+                        os.system(f"cp {checkpoint_path} sac_car_best_model.pth")
+
+                agent.update_parameters(buffer, batch_size)
+
+            if (total_steps == num_total_steps) or done or truncated:
+                break
+
+            #if done or truncated:
+            #    break
 
         if (total_steps == num_total_steps):
+            print(f"Training complete: {total_steps} steps")
             break
-
-        if done or truncated:
-            break
-
-    if (total_steps == num_total_steps):
-        break
 
